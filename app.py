c5816d700cba8fa0e94c40c19b20fedccb1ecf2f
Beshoy Hakeem <157409949+beshoyhakeem@users.noreply.github.com>
2025-07-10 23:03:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,33 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])   
 
+st.session_state.response_shown = False
 
-if Niche_query := st.chat_input("Campaign Requirements"):
+if Niche_query := st.chat_input("input Niche Required"):
     st.session_state.messages.append({"role":"user", "content": Niche_query})
     with st.chat_message("user"):
         st.markdown(Niche_query)
 
-    results = RAG_GPT(Niche_query,default_strategy)    
+    st.session_state.results = RAG_GPT(Niche_query,default_strategy)    
             
     with st.chat_message("AI"):
-        st.markdown(results["response"])
-        st.markdown(results["strategy"])
+        st.markdown(st.session_state.results["response"])
+        st.markdown(st.session_state.results["strategy"])
     
 
-    st.session_state.messages.append({"role":"AI","content":results["response"]})
+    st.session_state.messages.append({"role":"AI","content":st.session_state.results["response"]})
+    st.session_state.response_shown = True  # Show the button from now on
+
+# Show extra button after output is generated
+if st.session_state.response_shown and st.session_state.results:
+
+    st.download_button(
+    label="Download Personalized Emails as CSV",
+    data= csv_out(st.session_state.results["response"]).to_csv().encode('utf-8'),  # Convert DataFrame to CSV bytes
+    file_name='Personalized_Emails.csv',
+    mime='text/csv',
+    key="download_csv"
+)
 
 
           
